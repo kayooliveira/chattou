aba12c0c1692d7667dd8c034bb7dcf75ef5a7a5f
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-08 20:39:30 -0300

    feat(Adding baseUrl and rootDir to tsconfig.json): :sparkles:

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+import tsConfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   server: {
     port: 3000
   }
