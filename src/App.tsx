d4a70156729ed8bb7b822d6a6c1a63026ad1a221
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-08 18:41:41 -0300

    feat(Adding router to App.tsx): :sparkles:
    ]

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { BrowserRouter } from 'react-router-dom'
+
+import { Router } from './router'
 export function App() {
-  return <>Hello World</>
+  return (
+    <>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </>
+  )
 }
