8e571606ab1d1e774ec593ae64115e6dffbe8137
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-08 00:32:18 -0300

    feat(Updating to react 18): :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import { App } from "./App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.render(
+import './index.css'
+import { App } from './App'
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const container = document.getElementById('root')!
+
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  </React.StrictMode>
+)
