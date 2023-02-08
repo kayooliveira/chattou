503c488e358cf034f5680c0fbf9e4b6ceaabe411
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-08 18:40:37 -0300

    feat(Adding fontFamily.brand and backgroundImage.home to tailwind.config): :sparkles:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     extend: {
+      backgroundImage: {
+        home: 'url(/src/assets/img/bg-home.png)'
+      },
       colors: {
         app: { // App colors
             primary: "#7C01F6",
@@ -10,6 +13,9 @@ module.exports = {
             light: "#C8C8C8",
             text: "#FFFFFF"
         },
+      },
+      fontFamily: {
+        brand: 'Comfortaa, Arial, Helvetica, sans-serif'
       }
     },
   },
