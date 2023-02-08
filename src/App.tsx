3bd331e84a08c19869fd7317649f7f0765783a9c
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-08 20:41:51 -0300

    feat(Adding toaster provider to app.tsx): :sparkles:

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Router } from './router'
+
 export function App() {
   return (
     <>
+      <Toaster />
       <BrowserRouter>
         <Router />
       </BrowserRouter>
