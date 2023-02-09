c09dc209c0b5c92272eefc9d1ecf4ab085a38c7e
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-09 01:11:52 -0300

    feat(Updating Chat page): :sparkles:

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,20 +1,13 @@
-import { useAuthStore } from 'store/auth'
+import { Footer } from './components/Footer'
+import { Header } from './components/Header'
+import { RecentConversations } from './components/RecentConversations'
 
 export function Chat() {
-  const user = useAuthStore(state => state.user)
   return (
-    <div className="items start flex h-screen w-screen flex-col items-center justify-center gap-8 bg-home bg-cover bg-no-repeat font-brand text-app-text">
-      <div className="flex items-center justify-center gap-8 rounded-md bg-app-background p-4">
-        <img
-          src={user.profilePic}
-          referrerPolicy="no-referrer"
-          className="w-1/2 rounded-full"
-        />
-        <div className="flex flex-col items-center justify-center gap-1">
-          <p className="whitespace-nowrap text-xl uppercase">{user.name}</p>
-          <p className="text-app-light">@{user.username}</p>
-        </div>
-      </div>
+    <div className="flex max-h-screen max-w-full flex-col gap-4 bg-app-background p-8">
+      <Header />
+      <RecentConversations />
+      <Footer />
     </div>
   )
 }
