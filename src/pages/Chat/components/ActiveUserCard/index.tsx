11728effb9b59600c5bc64dd8ca188578258a618
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-10 03:00:59 -0300

    style(Updating styles from ActiveUserCards component): :sparkles:

--- src/pages/Chat/components/ActiveUserCard/index.tsx
+++ src/pages/Chat/components/ActiveUserCard/index.tsx
@@ -5,9 +5,13 @@ interface ActiveUserCardProps extends React.HTMLAttributes<HTMLDivElement> {
 
 export function ActiveUserCard({ profilePic, name }: ActiveUserCardProps) {
   return (
-    <div className="flex shrink-0 flex-col items-center justify-center gap-2">
-      <img src={profilePic} alt={name + ' user card'} />
-      <span className="tex-text text-md font-bold">{name}</span>
+    <div className="flex w-24 shrink-0 flex-col items-stretch justify-center gap-2 overflow-hidden">
+      <img
+        src={profilePic}
+        alt={name + ' user card'}
+        className="rounded-full"
+      />
+      <p className="truncate text-center font-bold">{name}</p>
     </div>
   )
 }
