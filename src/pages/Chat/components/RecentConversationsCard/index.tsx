5a0cacba6903009ef3dad6c579b0ff35342d4216
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-10 02:59:48 -0300

    style(Updating styles to RecentConversationCard and adding profilePic prop): :sparkles:

--- src/pages/Chat/components/RecentConversationsCard/index.tsx
+++ src/pages/Chat/components/RecentConversationsCard/index.tsx
@@ -1,28 +1,29 @@
-import friend from 'assets/img/friends/friend1.png'
 interface RecentConversationsCardProps {
   name: string
   lastMessage: string
   lastMessageTime: string
   messagesQnt: number
+  profilePic: string
 }
 export function RecentConversationsCard({
   name,
   lastMessage,
   lastMessageTime,
-  messagesQnt
+  messagesQnt,
+  profilePic
 }: RecentConversationsCardProps) {
   return (
-    <div className="flex items-center justify-between rounded-3xl border border-app-text/10 bg-app-backgroundLight p-4">
-      <div className="flex items-center justify-center gap-2">
-        <img src={friend} className="rounded-full" />
-        <div className="flex flex-col gap-2">
-          <span className="text-app-text">{name}</span>
-          <span className="font-bold text-app-text">{lastMessage}</span>
-        </div>
+    <div className="flex items-center justify-between gap-2 rounded-[2rem] border border-app-text/10 bg-app-backgroundLight p-4 text-xs transition-colors hover:border-app-text/30 hover:bg-app-backgroundLight/20 md:gap-4 md:text-sm lg:text-base">
+      <img src={profilePic} className="w-[23%] rounded-full" />
+      <div className="flex flex-1 flex-col gap-4 overflow-hidden">
+        <p className="truncate text-sm font-bold text-app-text md:text-base">
+          {name}
+        </p>
+        <p className="truncate text-app-light">{lastMessage}</p>
       </div>
-      <div className="flex flex-col gap-2">
-        <span className="text-app-primary">{lastMessageTime}</span>
-        <span className="flex h-4 w-4 items-center justify-center rounded-full bg-app-primary font-bold text-app-text">
+      <div className="flex shrink-0 flex-col items-end gap-2">
+        <p className="text-xs text-app-primary">{lastMessageTime}</p>
+        <span className="flex h-4 w-4 items-center justify-center rounded-full bg-app-primary text-xs font-bold text-app-text">
           {messagesQnt}
         </span>
       </div>
