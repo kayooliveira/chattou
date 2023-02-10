264b3f25e4b3b24c2b1e3184b1239a9c265d5cd3
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-10 03:01:28 -0300

    feat(Hidding footer on large screens): :sparkles:

--- src/pages/Chat/components/Footer/index.tsx
+++ src/pages/Chat/components/Footer/index.tsx
@@ -7,7 +7,7 @@ import {
 
 export function Footer() {
   return (
-    <footer className="fixed bottom-4 left-1/2 flex h-fit w-[calc(100%_-_4rem)] -translate-x-1/2 translate-y-0 items-center justify-center gap-8 rounded-full bg-app-backgroundLight px-8 py-4 text-app-light shadow-md shadow-black drop-shadow-2xl">
+    <footer className="fixed bottom-4 left-1/2 flex h-fit w-[calc(100%_-_4rem)] -translate-x-1/2 translate-y-0 items-center justify-center gap-8 rounded-full bg-app-backgroundLight px-8 py-4 text-app-light shadow-md shadow-black drop-shadow-2xl lg:hidden">
       <button className="transition-colors hover:text-app-primary">
         <IoMdChatbubbles size="30" />
       </button>
