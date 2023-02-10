e8effb43a33a690191c34f4037ee377b338cec20
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-10 03:00:26 -0300

    feat(Now the Chat component load all the user chats onload): :sparkles:

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,12 +1,71 @@
+import {
+  collection,
+  getDoc,
+  getDocs,
+  query,
+  doc,
+  where
+} from 'firebase/firestore'
+import { database } from 'lib/firebase'
+import { useEffect } from 'react'
+import { useAuthStore } from 'store/auth'
+import { Chat as IChat, useChatStore } from 'store/chat'
+
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { RecentConversations } from './components/RecentConversations'
 
 export function Chat() {
+  const user = useAuthStore(state => state.user)
+  const setChats = useChatStore(state => state.setChats)
+  useEffect(() => {
+    if (user) {
+      const getChats = async () => {
+        const chatsRef = collection(database, 'chats')
+        const chatsQuery = query(
+          chatsRef,
+          where('users', 'array-contains', user.id)
+        )
+        const querySnap = await getDocs(chatsQuery)
+        const chats: IChat[] = []
+
+        querySnap.forEach(async document => {
+          if (document.exists()) {
+            const data = document.data()
+            if (data) {
+              const usersDoc = doc(database, 'users', data.users[1])
+              const user2Data = await getDoc(usersDoc).then(user2Doc => {
+                if (user2Doc.exists()) {
+                  const data = user2Doc.data()
+                  if (data) {
+                    return data
+                  }
+                }
+              })
+              const chat = {
+                lastMessage: data.lastMessage,
+                lastMessageDate: new Date(data.lastMessageDate.seconds * 1000),
+                name: data.name,
+                users: data.users,
+                image: user2Data?.profilePic
+              }
+              setChats([...chats, chat])
+            }
+          }
+        })
+      }
+
+      getChats()
+    }
+  }, [user])
+
   return (
-    <div className="flex max-h-screen max-w-full flex-col gap-4 bg-app-background p-8">
-      <Header />
-      <RecentConversations />
+    <div className="mx-auto flex h-full max-h-screen max-w-screen-2xl gap-4 bg-app-background p-8">
+      <div className="flex max-w-full flex-col md:w-2/5 lg:w-1/3 ">
+        <Header />
+        <RecentConversations />
+      </div>
+      <div className="hidden flex-1 bg-app-light p-4 lg:block"></div>
       <Footer />
     </div>
   )
