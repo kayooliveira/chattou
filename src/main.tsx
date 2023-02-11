26661d1c1e136d4db4933afd8de8cdb58d312de4
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-11 17:28:37 -0300

    feat(Removing strict mode): :sparkles:

--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,4 @@ const container = document.getElementById('root')!
 
 const root = createRoot(container)
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<App />)
