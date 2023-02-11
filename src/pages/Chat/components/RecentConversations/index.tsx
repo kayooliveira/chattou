750786f471efac03d8b868e72d04fc422eae22da
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-11 03:36:41 -0300

    feat(Adding key and chatId to RecentConversations): :sparkles:

--- src/pages/Chat/components/RecentConversations/index.tsx
+++ src/pages/Chat/components/RecentConversations/index.tsx
@@ -1,7 +1,6 @@
 import { format } from 'date-fns'
 import { IoMdClock } from 'react-icons/io'
 import { useChatStore } from 'store/chat'
-import { v4 } from 'uuid'
 
 import { RecentConversationsCard } from '../RecentConversationsCard'
 
@@ -18,7 +17,8 @@ export function RecentConversations() {
           {chats &&
             chats.map(chat => (
               <RecentConversationsCard
-                key={v4()}
+                key={chat.id}
+                chatId={chat.id}
                 name={chat.name}
                 lastMessage={chat.lastMessage}
                 lastMessageTime={format(chat.lastMessageDate, 'dd/MM/yyyy')}
@@ -26,35 +26,6 @@ export function RecentConversations() {
                 profilePic={chat.image}
               />
             ))}
-
-          <RecentConversationsCard
-            name="Kaline (Luna)"
-            lastMessage="Isso q n pode!"
-            lastMessageTime="2 min"
-            messagesQnt={2}
-            profilePic="https://pps.whatsapp.net/v/t61.24694-24/321173410_582449893307395_5220122229470741790_n.jpg?ccb=11-4&oh=01_AdSAqt5Cbcg2TxD13dPIteucSA66BAOi3kvxi349Ip4Qag&oe=63F2CA0B"
-          />
-          <RecentConversationsCard
-            name="Savio"
-            lastMessage="Randolar randolar randolar randolar"
-            lastMessageTime="5 min"
-            messagesQnt={1}
-            profilePic="https://pps.whatsapp.net/v/t61.24694-24/325793715_962615208055108_9002868011396268687_n.jpg?ccb=11-4&oh=01_AdTEIJ8jkRQiIq3JsP4K7o8PeUfGVIsA77vFPKjoAk3xNA&oe=63F2C27D"
-          />
-          <RecentConversationsCard
-            name="Berenício"
-            lastMessage="Eita como ta de boa!"
-            lastMessageTime="4 min"
-            messagesQnt={1}
-            profilePic="https://pps.whatsapp.net/v/t61.24694-24/316452413_693609895767454_6516523516504109189_n.jpg?ccb=11-4&oh=01_AdQMSdyiN71qGhvbvhJZxAOZgtBGPQcBhwq1gHufm6Us8Q&oe=63F2B9A5"
-          />
-          <RecentConversationsCard
-            name="Eliza"
-            lastMessage="Ta bom xuxu ❤"
-            lastMessageTime="6 min"
-            messagesQnt={2}
-            profilePic="https://pps.whatsapp.net/v/t61.24694-24/324681967_204867555446011_7520175057960077826_n.jpg?ccb=11-4&oh=01_AdTQKNenlC32irGonGlKr41C0qHvspPXqCH8SABu49FflA&oe=63F293DA"
-          />
           <div className="h-20 w-full" />
         </div>
       </div>
