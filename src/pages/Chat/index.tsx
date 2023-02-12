6df339b27e02f95ed1cd6da72c0ad8cd46b889eb
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-12 03:12:38 -0300

    feat(Updating width based on the  isCurrentConversationOpen state value): :art:

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,12 +1,26 @@
+import classNames from 'classnames'
+import { useConversationStore } from 'store/conversation'
+
 import { CurrentConversation } from './components/CurrentConversation'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { RecentConversations } from './components/RecentConversations'
 
 export function Chat() {
+  const isCurrentConversationOpen = useConversationStore(
+    state => state.isCurrentConversationOpen
+  )
   return (
     <div className="mx-auto flex h-screen max-h-screen max-w-screen-2xl gap-4 bg-app-background p-8">
-      <div className="relative flex h-full w-full max-w-full flex-col md:w-2/5 lg:w-1/3">
+      <div
+        className={classNames(
+          'relative flex h-full w-full max-w-full flex-col',
+          {
+            'w-full md:w-full lg:w-full': !isCurrentConversationOpen,
+            'md:w-2/5 lg:w-1/3': isCurrentConversationOpen
+          }
+        )}
+      >
         <Header />
         <RecentConversations />
       </div>
