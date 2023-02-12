ec5fd09d8f4b3c3f037fbf6631d7daa884529817
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-12 16:37:14 -0300

    feat(Adding new prop id.): :sparkles: Now the user can click on active users to start a new conversation

--- src/pages/Chat/components/ActiveUserCard/index.tsx
+++ src/pages/Chat/components/ActiveUserCard/index.tsx
@@ -1,10 +1,14 @@
+import { useAuthStore } from 'store/auth'
+import { useConversationStore } from 'store/conversation'
+
 interface ActiveUserCardProps extends React.HTMLAttributes<HTMLDivElement> {
   avatar: string
   name: string
+  id: string
 }
 
 /**
- * @version 0.0.1
+ * @version 0.0.2
  *
  * @author Kayo Oliveira <contato@kayooliveira.com>
  *
@@ -13,16 +17,38 @@ interface ActiveUserCardProps extends React.HTMLAttributes<HTMLDivElement> {
  * @param { ActiveUserCardProps } user Os dados do usuário.
  * @param { string } user.avatar URL do avatar do usuário.
  * @param { string } user.name Nome do usuário.
+ * @param { string } user.id ID do usuário.
  *
  * @return ReactElement
  */
 
 export function ActiveUserCard({
   avatar,
-  name
+  name,
+  id
 }: ActiveUserCardProps): React.ReactElement {
+  const user = useAuthStore(state => state.user)
+  const createConversation = useConversationStore(
+    state => state.createConversation
+  )
+
+  /**
+   * @version 0.0.1
+   *
+   * @author Kayo Oliveira <contato@kayooliveira.com>
+   *
+   * @description Cria uma nova conversa ao clicar no card do usuário.
+   *
+   * @return void
+   */
+  function handleCreateNewConversation(): void {
+    createConversation(user.id, id)
+  }
   return (
-    <div className="flex w-16 shrink-0 cursor-pointer flex-col items-stretch justify-center gap-2 overflow-hidden">
+    <div
+      onClick={handleCreateNewConversation}
+      className="flex w-16 shrink-0 cursor-pointer flex-col items-stretch justify-center gap-2 overflow-hidden"
+    >
       <img src={avatar} alt={name + ' user card'} className="rounded-full" />
       <p className="truncate text-center text-xs font-bold lg:text-base">
         {name}
