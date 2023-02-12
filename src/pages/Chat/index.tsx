fa61544321852b0b8486f87def38970c657ab441
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-12 20:31:42 -0300

    feat(Ask user to allow the site send notifications.): :sparkles:

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
+
 import { CurrentConversation } from './components/CurrentConversation'
 import { Footer } from './components/Footer'
 import { Header } from './components/Header'
 import { RecentConversations } from './components/RecentConversations'
 
 export function Chat() {
+  useEffect(() => {
+    if (!('Notification' in window)) {
+      console.log('This browser does not support desktop notification')
+    } else {
+      Notification.requestPermission()
+    }
+  }, [])
+
   return (
     <div className="mx-auto flex h-screen max-h-screen max-w-screen-2xl gap-4 bg-app-background p-8">
       <section className="relative flex max-h-full w-full max-w-full flex-col overflow-hidden lg:w-auto">
