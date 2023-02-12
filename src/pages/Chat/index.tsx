e03e0ff373e3c02176a27e3267b30e209fd45de8
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-12 02:14:39 -0300

    style(Updating width from Chat component): :art:

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -6,7 +6,7 @@ import { RecentConversations } from './components/RecentConversations'
 export function Chat() {
   return (
     <div className="mx-auto flex h-screen max-h-screen max-w-screen-2xl gap-4 bg-app-background p-8">
-      <div className="relative flex h-full max-w-full flex-col md:w-2/5 lg:w-1/3">
+      <div className="relative flex h-full w-full max-w-full flex-col md:w-2/5 lg:w-1/3">
         <Header />
         <RecentConversations />
       </div>
