672bbd64159113c5c9181c9ba22977df019bbd4e
Kayo Oliveira <g.kayooliveira123@gmail.com>
2023-02-12 00:27:41 -0300

    refactor(Refactoring and adding documentation to ActiveUserCard component): :memo: :recycle:

--- src/pages/Chat/components/ActiveUserCard/index.tsx
+++ src/pages/Chat/components/ActiveUserCard/index.tsx
@@ -1,17 +1,32 @@
 interface ActiveUserCardProps extends React.HTMLAttributes<HTMLDivElement> {
-  profilePic: string
+  avatar: string
   name: string
 }
 
-export function ActiveUserCard({ profilePic, name }: ActiveUserCardProps) {
+/**
+ * @version 0.0.1
+ *
+ * @author Kayo Oliveira <contato@kayooliveira.com>
+ *
+ * @description 'Card' contendo avatar e nome do usuário definido.
+ *
+ * @param { ActiveUserCardProps } user Os dados do usuário.
+ * @param { string } user.avatar URL do avatar do usuário.
+ * @param { string } user.name Nome do usuário.
+ *
+ * @return ReactElement
+ */
+
+export function ActiveUserCard({
+  avatar,
+  name
+}: ActiveUserCardProps): React.ReactElement {
   return (
-    <div className="flex w-16 shrink-0 flex-col items-stretch justify-center gap-2 overflow-hidden">
-      <img
-        src={profilePic}
-        alt={name + ' user card'}
-        className="rounded-full"
-      />
-      <p className="truncate text-center font-bold">{name}</p>
+    <div className="flex w-16 shrink-0 cursor-pointer flex-col items-stretch justify-center gap-2 overflow-hidden">
+      <img src={avatar} alt={name + ' user card'} className="rounded-full" />
+      <p className="truncate text-center text-xs font-bold lg:text-base">
+        {name}
+      </p>
     </div>
   )
 }
